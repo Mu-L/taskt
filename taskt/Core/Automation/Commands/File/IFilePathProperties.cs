04b2dab2924a70d6a798d59f3812dd96a9a6c98d
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-01-04 14:22:20 +0900

    add interfaces, abstract classes

---
+++ taskt/Core/Automation/Commands/File/IFilePathProperties.cs
@@ -0,0 +1,13 @@
+﻿namespace taskt.Core.Automation.Commands
+{
+    /// <summary>
+    /// for to specifiy file path properties
+    /// </summary>
+    public interface IFilePathProperties : ILExpandableProperties
+    {
+        /// <summary>
+        /// target file path
+        /// </summary>
+        string v_TargetFilePath { get; set; }
+    }
+}
