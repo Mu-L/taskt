f242fec987de8e1afd8814bafaf0e7d78d7f83fb
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-02-02 21:00:21 +0900

    add interface

---
+++ taskt/Core/Automation/Commands/Folder/ICanHandleFolderName.cs
@@ -0,0 +1,10 @@
+﻿namespace taskt.Core.Automation.Commands.Folder
+{
+    /// <summary>
+    /// for folder name
+    /// </summary>
+    public interface ICanHandleFolderName : ICanHandleFileOrFolderName
+    {
+        // nothing
+    }
+}
