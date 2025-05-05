1d244078b0d8973a0aabd78fb0e118f8d8477987
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-05-05 21:55:15 +0900

    use virtualproperty

--- taskt/Core/Automation/Commands/NLGControls.cs
+++ taskt/Core/Automation/Commands/NLGControls.cs
@@ -5,19 +5,23 @@ namespace taskt.Core.Automation.Commands
 {
     internal static class NLGControls
     {
+        /// <summary>
+        /// NLG Instance Name
+        /// </summary>
+        [PropertyVirtualProperty(nameof(GeneralPropertyControls), nameof(GeneralPropertyControls.v_InputInstanceName))]
         [PropertyDescription("NLG Instance Name")]
-        [InputSpecification("NLG Instance Name", true)]
+        [InputSpecification("NLG Instance Name")]
         [PropertyDetailSampleUsage("**nlgInstance**", PropertyDetailSampleUsage.ValueType.Value, "Instance")]
         [PropertyDetailSampleUsage("**{{{vInstance}}}**", PropertyDetailSampleUsage.ValueType.VariableValue, "Instance")]
-        [PropertyShowSampleUsageInDescription(true)]
-        [Remarks("Failure to enter the correct instance name or failure to first call **Create NLG Instance** command will cause an error")]
-        [PropertyInstanceType(PropertyInstanceType.InstanceType.NLG)]
-        [PropertyRecommendedUIControl(PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
-        [PropertyParameterDirection(PropertyParameterDirection.ParameterDirection.Input)]
         [PropertyValidationRule("NLG Instance", PropertyValidationRule.ValidationRuleFlags.Empty)]
         [PropertyDisplayText(true, "Instance")]
         [PropertyFirstValue("%kwd_default_nlg_instance%")]
-        [PropertyParameterOrder(5000)]
+        [Remarks("Failure to enter the correct instance name or failure to first call **Create NLG Instance** command will cause an error")]
+        [PropertyInstanceType(PropertyInstanceType.InstanceType.NLG)]
+        //[PropertyShowSampleUsageInDescription(true)]
+        //[PropertyParameterDirection(PropertyParameterDirection.ParameterDirection.Input)]
+        //[PropertyRecommendedUIControl(PropertyRecommendedUIControl.RecommendeUIControlType.ComboBox)]
+        //[PropertyParameterOrder(5000)]
         public static string v_InstanceName { get; }
     }
 }
