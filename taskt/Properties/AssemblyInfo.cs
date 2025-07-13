68c5f4b61a316984d04b2b61c66f0625984bff57
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-07-13 18:32:21 +0900

    fix url etc

--- taskt/Properties/AssemblyInfo.cs
+++ taskt/Properties/AssemblyInfo.cs
@@ -17,11 +17,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("taskt uob")]
+[assembly: AssemblyTitle("taskt")]
 [assembly: AssemblyDescription("Build and Execute Automation Scripts")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("taskt uob")]
+[assembly: AssemblyProduct("taskt")]
 [assembly: AssemblyCopyright("Copyright ©2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -44,6 +44,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("0.0.*")]
-[assembly: AssemblyFileVersion("3.5.2.34")]
+[assembly: AssemblyFileVersion("3.6.0.0")]
 //[assembly: AssemblyVersion("0.0.0.2")]
 //[assembly: AssemblyFileVersion("0.0.0.2")]
