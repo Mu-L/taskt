d71aab831ff0659b75cecf98bd5ff4550cffcc41
rcktrncn <48382442+rcktrncn@users.noreply.github.com>
2025-07-13 19:04:43 +0900

    add partial class

--- taskt/Core/MyURLs.cs
+++ taskt/Core/MyURLs.cs
@@ -1,17 +1,7 @@
 ﻿namespace taskt.Core
 {
-    public class MyURLs
+    public partial class MyURLs
     {
-        public const string GitProjectURL = "https://github.com/saucepleez/taskt";
-        public const string GitReleaseURL = "https://github.com/saucepleez/taskt/releases";
-        public const string GitIssueURL = "https://github.com/saucepleez/taskt/issues/new";
-        public const string WikiURL = "https://github.com/saucepleez/taskt-wiki/blob/master/home.md";
-        public const string OfficialSiteURL = "http://www.taskt.net/";
-        public const string GitterURL = "https://gitter.im/taskt-rpa/Lobby";
-        public const string WikiBaseURL = "https://github.com/saucepleez/taskt-wiki/blob/master/";
-
-        public const string LatestJSONURL = "http://www.taskt.net/updates/latest.json";
-
         public const string ChromeDriverURL = "https://googlechromelabs.github.io/chrome-for-testing/";
         public const string EdgeDriverURL = "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/";
         public const string GeckoDriverURL = "https://github.com/mozilla/geckodriver/releases/";
